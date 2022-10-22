e1920f836e91bc6bfe3efb89012b6a87d3c03f36
houssem janfaoui <81567436+hoss90@users.noreply.github.com>
2022-10-22 19:27:51 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,9 @@ function Footer(){
         <div className="footer">
             <footer>
       <div className="social-media">
-      <a className="social-footer" class="profile" rel="noreferrer" href="https://www.instagram.com/houssem_janfaoui/" target="_blank"><img src="https://img.icons8.com/ios-glyphs/40/000000/instagram-new.png" className="social"/></a>
-      <a className="social-footer" rel="noreferrer" href="https://github.com/hoss90" target="_blank"><img src="https://img.icons8.com/material-rounded/40/000000/github.png" className="social"/></a>
-      <a className="social-footer" rel="noreferrer" href="https://www.linkedin.com/in/houssem-janfaoui-a0b687103/" target="_blank"><img src="https://img.icons8.com/ios-glyphs/40/000000/linkedin-circled--v1.png" className="social"/></a>
+      <a className="social-footer" class="profile" rel="noreferrer" href="https://www.instagram.com/houssem_janfaoui/" target="_blank"><img src="https://img.icons8.com/ios-glyphs/40/000000/instagram-new.png" alt="insta" className="social"/></a>
+      <a className="social-footer" rel="noreferrer" href="https://github.com/hoss90" target="_blank"><img src="https://img.icons8.com/material-rounded/40/000000/github.png" alt="github" className="social"/></a>
+      <a className="social-footer" rel="noreferrer" href="https://www.linkedin.com/in/houssem-janfaoui-a0b687103/" target="_blank"><img src="https://img.icons8.com/ios-glyphs/40/000000/linkedin-circled--v1.png" alt="linkedin" className="social"/></a>
     </div>
     <div className="link-footer">
     <span class="left"> <a href="#home"className="link">Home</a></span>
